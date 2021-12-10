17bc712b13b944f26985870bd567835a1966b2f7
Serhii Tyshchenko (EPAM) <serhii_tyshchenko1@epam.com>
2021-12-10 23:59:01 +0200

    add event handler logic

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
 import 'index.scss';
 import App from './app';
 
+export const eventHandlers = [];
+
 const OUTPUT = document.getElementById('root');
+
+OUTPUT.addEventListener('click', (e) => {
+  if (!e.target?.id) return null;
+
+  const targetId = e.target.id;
+  const pair = eventHandlers.find((el) => el.id === targetId);
+  if (pair) {
+    pair.onClick && pair.onClick(e);
+  }
+});
+
+OUTPUT.addEventListener('change', (e) => {
+  if (!e.target?.id) return null;
+
+  const targetId = e.target.id;
+  const pair = eventHandlers.find((el) => el.id === targetId);
+  if (pair) {
+    pair.onChange && pair.onChange(e);
+  }
+});
 
 OUTPUT.innerHTML = App();
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,27 @@
 import { Header, Main, Footer, Container } from 'layout';
+import { UIButton, UIInput } from 'components/atoms';
 
 export const Home = () => {
-  const children = `${Header()}${Main()}${Footer()}`;
-  return Container({ children });
+  const handleClick = (evt) => console.log(evt.target?.id);
+  const handleChange = (evt) => console.log(evt.target?.value);
+
+  return Container({
+    children: `
+    ${Header()}
+    ${Main({
+      children: `
+        ${UIButton({
+          label: 'adsd',
+          id: 'id1',
+          onClick: handleClick,
+        })}
+      ${UIButton({
+        id: 'id2',
+        onClick: handleClick,
+      })}
+      ${UIInput({ onChange: handleChange })}`,
+    })}
+    ${Footer()}
+    `,
+  });
 };
